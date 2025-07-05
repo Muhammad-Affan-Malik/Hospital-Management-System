1816b938f01eb6f8c2ab720baefb4a8de453687f
Muhammad-Affan-Malik <muhammadaffanawan707@gmail.com>
2025-07-05 23:52:07 +0500

    Creating Loggs formatter

---
+++ HumanReadableLogFormatter.cs
@@ -0,0 +1,16 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+using System;
+using System.IO;
+
+public class HumanReadableLogFormatter : ITextFormatter
+{
+    public void Format(LogEvent logEvent, TextWriter output)
+    {
+        var date = logEvent.Timestamp.ToString("yyyy-MM-dd");
+        var level = logEvent.Level.ToString().ToUpper();
+        var message = logEvent.RenderMessage();
+
+        output.WriteLine($"[{date}] {level}: {message}");
+    }
+}
