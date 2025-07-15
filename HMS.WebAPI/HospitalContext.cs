de49771726923e26370949290ef58da97c713ec3
Muhammad-Affan-Malik <muhammadaffanawan707@gmail.com>
2025-07-15 20:12:32 +0500

    HMS.WebAPI added

---
+++ HMS.WebAPI/HospitalContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HMS.WebAPI.Controllers;
+using HMS.WebAPI.Models; // Ensure this matches where your Patient.cs is
+
+namespace HMS.WebAPI
+{
+    public class HospitalContext : DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
---
+++ HMS.WebAPI/Models/Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.WebAPI.Models
+
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public required string Gender { get; set; }
+        public required string Disease { get; set; }
+        public DateTime AdmissionDate { get; set; }
+    }
+
+}
